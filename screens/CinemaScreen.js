b695e1a472214f8051c186392982f18445a874c2
Rebecca Grenet <rebeccagnt@orange.fr>
2023-04-07 16:18:31 +0200

    update import

--- screens/CinemaScreen.js
+++ screens/CinemaScreen.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, View, Button, StyleSheet } from "react-native";
 import styles from "../theme/styles";
-import Card from "../components/Card";
+import CinemaCard from "../components/CinemaCard";
 
 const CinemaScreen = ({ navigation }) => {
   const onPressCinema = () => {
@@ -11,7 +11,7 @@ const CinemaScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Les cinémas</Text>
-      <Card onPress={() => onPressCinema()} />
+      <CinemaCard onPress={() => onPressCinema()} />
     </View>
   );
 };
